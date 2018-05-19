2a34d20233c1fa22fa7d9732facc1c9df3858c4f
Rinat Abdullin <rinat.abdullin@gmail.com>
2018-05-19 09:52:59 +0200

    packer: install mosh to the tester image

--- packer-tester/upload/ubuntu.sh
+++ packer-tester/upload/ubuntu.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+set -x
 
 # from http://unix.stackexchange.com/a/28793
 # if we aren't root - elevate. This is useful for AMI
@@ -16,8 +17,11 @@ dpkg-reconfigure tzdata
 
 # need to clean since images could have stale metadata
 apt-get clean && apt-get update
-apt-get -qq -y install build-essential libssl-dev git python curl wget htop screen \
-    && apt-get clean && rm -rf /var/lib/apt/lists/*
+apt-get -qq -y install build-essential libssl-dev git python curl wget htop mosh screen
+
+
+#cleanup
+apt-get clean && rm -rf /var/lib/apt/lists/*
 
 
 ########## Java Runtime
@@ -31,7 +35,7 @@ apt-get -qq -y install build-essential libssl-dev git python curl wget htop scre
 
 cd /tmp
 
-wget https://www.foundationdb.org/downloads/5.1.7/ubuntu/installers/foundationdb-clients_5.1.7-1_amd64.deb
+wget -nv https://www.foundationdb.org/downloads/5.1.7/ubuntu/installers/foundationdb-clients_5.1.7-1_amd64.deb
 dpkg -i foundationdb-clients_5.1.7-1_amd64.deb
 
 # create the folder if it doesn't exist
